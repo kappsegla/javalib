6620cc8ff3170bd4b198aa6fe9fe8f78f3a5413a
Martin Blomberg <martin.blomberg@outlook.com>
2026-01-21 09:13:43 +0100

    Adds Calculator

---
+++ src/main/java/com/example/gitdemo/Calculator.java
@@ -0,0 +1,19 @@
+package com.example.gitdemo;
+
+public class Calculator {
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+    public static int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public static int add(int... numbers){
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+}
